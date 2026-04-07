db16953131bd21b17d81aa9ebec88d993479409f
githun367 <jasminagar21@gmail.com>
2026-04-07 10:30:26 +0200

    Sat Main op(javalin), config og frontend struktur

---
+++ src/main/java/Main.java
@@ -0,0 +1,22 @@
+import config.SessionConfig;
+import config.ThymeleafConfig;
+import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinThymeleaf;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Initializing Javalin and Jetty webserver
+        Javalin app = Javalin.create(config -> {
+            config.staticFiles.add("/public");
+            config.jetty.modifyServletContextHandler(
+                    handler -> handler.setSessionHandler(SessionConfig.sessionConfig())
+            );
+            config.fileRenderer(new JavalinThymeleaf(ThymeleafConfig.templateEngine()));
+        }).start(7070);
+
+        // Routing
+        app.get("/", ctx -> ctx.render("index.html"));
+    }
+}
